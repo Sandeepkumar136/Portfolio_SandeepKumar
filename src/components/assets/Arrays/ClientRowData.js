29c044f1b67bc8ae89f31583097b37aa053dabc1
Sandeep <Ksundip750@gmail.com>
2025-07-27 23:42:12 +0530

    Add GitHub Activity Chart Component with Event Fetching and Visualization

---
+++ src/components/assets/Arrays/ClientRowData.js
@@ -0,0 +1,51 @@
+const Client_Data = [
+  {
+    client: "Rajiv Punia",
+    img: require("../Pictures/client-3.jpg"),
+    star: "4.5",
+    description: "seamless user experiences.",
+  },
+  {
+    client: "Mahesh Chandra",
+    img: require("../Pictures/client-2.webp"),
+    star: "4",
+    description: "responsive designs and excellent user interactions.",
+  },
+  {
+    client: "Samanta Kashyap",
+    img: require("../Pictures/client-1.jpg"),
+    star: "4",
+    description: "Custom features and responsive layouts",
+  },
+  {
+    client: 'Virat Gupta',
+    img: require('../Pictures/client-5.jpg'),
+    star: '5',
+    description: 'responsive design were well-executed.'
+  },
+  {
+    client: 'Brittany Kaufman',
+    img: require('../Pictures/client-4.jpg'),
+    star: '3',
+    description: 'Have to More Speed and Efficency'
+  },
+  {
+    client: 'Courtney Sniadecki',
+    img: require('../Pictures/client-6.jpg'),
+    star: '3.5',
+    description: 'Reliable Framework',
+  },
+  {
+    client: 'Tyler Sanslow',
+    img: require('../Pictures/client-7.jpg'),
+    star: '5',
+    description: 'Efficient React development',
+  },
+  {
+    client: 'Justing Sires',
+    img: require('../Pictures/client-8.jpg'),
+    star: '4',
+    description: 'Responsive layouts'
+  }
+];
+export default Client_Data;
---
+++ src/components/assets/Pictures/ImageProvider.js
@@ -0,0 +1,23 @@
+const Images={
+    Resume: require('./SandeepResume.pdf'),
+    Rimg1: require('./SandeepResume-1.jpg'),
+    Rimg2: require('./SandeepResume-2.jpg'),
+    Rimg3: require('./SandeepResume-3.jpg'),
+    Rimg4: require('./SandeepResume-4.jpg'),
+    Rimg5: require('./SandeepResume-5.jpg'),
+    DataManagement: require('./frontend.jpg'),
+    Frontend: require('./2150062008-1.jpg'),
+    Backend: require('./3642-1.jpg'),
+    Illustration_Frontend: require('./freepik__background__61590.png'),
+    Partners: {
+        Partner_One: require('./partner-1.jpg'),
+        Partner_Two: require('./partner-2.jpg'),
+        Partner_Three: require('./partner-3.jpg'),
+        Partner_four: require('./review-5.webp'),
+    },
+    git_background: require('./2150248019.jpg'),
+    sandeep_one: require('./sandeep_img1.jpg'),
+    form_background: require('./vadim-shevyrin-xsTDS2URj6c-unsplash.jpg'),
+
+}
+export default Images;
